3e1aa298716d08dfea4da06fd2e1aa131eed1d13
Gowreesh V T <135145326+Gowreesh-VT@users.noreply.github.com>
2026-04-20 11:16:28 +0530

    refactor: clean up _app.tsx and update tsconfig.json for better type support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     // The token is hardcoded and contains a flag in its payload
     const jwtToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZ3Vlc3QiLCJyb2xlIjoiY3VzdG9tZXIiLCJmbGFnIjoiVlZ7and3N19kM2MwZDNkXzV1Y2MzNTV2dWxseX0ifQ.A2bC3dE4fG5hI6jK7lM8nO9pQ0rS1tU2vW3xY4zA5bC';
-    
-    // Set the cookie (visible in browser DevTools)
+
     Cookies.set('token', jwtToken, { expires: 7 });
   }, []);
 
